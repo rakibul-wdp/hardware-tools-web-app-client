8889f05a26af507a1887333c2f69810bd7450ba1
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-24 08:58:25 +0600

    make admin and conditionaly add class

--- src/Pages/Dashboard/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useQuery } from 'react-query';
+import { toast } from 'react-toastify';
 import Loading from '../Shared/Loading';
 
 const MakeAdmin = () => {
@@ -18,6 +19,28 @@ const MakeAdmin = () => {
   if (isLoading) {
     return <Loading></Loading>;
   }
+
+  const makeAdmin = (email) => {
+    fetch(`http://localhost:5000/user/admin/${email}`, {
+      method: 'PUT',
+      headers: {
+        authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      },
+    })
+      .then((res) => {
+        if (res.status === 403) {
+          toast.error('Failed to Make an admin');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          refetch();
+          toast.success('Successfully made an admin');
+        }
+      });
+  };
+
   return (
     <div>
       <h2>Make Admin {users.length}</h2>
@@ -28,16 +51,28 @@ const MakeAdmin = () => {
               <th></th>
               <th>Email</th>
               <th>Job</th>
-              <th>Favorite Color</th>
+              <th>Make Admin</th>
             </tr>
           </thead>
           <tbody>
             {users.map((user, index) => (
               <tr key={user._id}>
                 <th>{index + 1}</th>
-                <td>{user.email}</td>
+                {user.role === 'admin' ? (
+                  <td className='tooltip tooltip-open tooltip-right' data-tip='Admin'>
+                    {user.email}
+                  </td>
+                ) : (
+                  <td>{user.email}</td>
+                )}
                 <td>Quality Control Specialist</td>
-                <td>Blue</td>
+                <td>
+                  {user.role !== 'admin' && (
+                    <button onClick={() => makeAdmin(user.email)} className='btn btn-xs'>
+                      Make Admin
+                    </button>
+                  )}
+                </td>
               </tr>
             ))}
           </tbody>
