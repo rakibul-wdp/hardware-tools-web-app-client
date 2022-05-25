d5789e62a7099a89eb13014c1b41b683a8f5bb52
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-25 18:32:23 +0600

    add profile data and update also

--- src/Pages/Dashboard/MyProfile.js
+++ src/Pages/Dashboard/MyProfile.js
@@ -1,25 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import UpdateProfileModal from './UpdateProfileModal';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
 
 const MyProfile = () => {
-  const [profiles, setProfiles] = useState({});
   const [user] = useAuthState(auth);
 
-  useEffect(() => {
-    if (user) {
-      fetch(`http://localhost:5000/profile?email=${user.email}`, {
-        method: 'GET',
-        headers: {
-          authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-        },
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          setProfiles(data);
-        });
-    }
-  }, [user]);
+  const {
+    data: profiles,
+    isLoading,
+    refetch,
+  } = useQuery('profile', () =>
+    fetch(`http://localhost:5000/profile?email=${user.email}`, {
+      method: 'GET',
+      headers: {
+        authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      },
+    }).then((res) => res.json())
+  );
+
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <div className='w-11/12 mx-auto my-10 shadow-lg p-10'>
       <h2 className='text-center font-bold text-xl text-primary'>My Profile</h2>
@@ -49,6 +53,10 @@ const MyProfile = () => {
           <p className='border outline-1 py-2 px-3 bg-gray-200'>{profiles[0]?.linkedin}</p>
         </div>
       </div>
+      <label htmlFor='profile-modal' className='btn btn-primary'>
+        Update Profile
+      </label>
+      {<UpdateProfileModal profiles={profiles} refetch={refetch} />}
     </div>
   );
 };
---
+++ src/Pages/Dashboard/UpdateProfileModal.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
+import auth from '../../firebase.init';
+
+const UpdateProfileModal = ({ profiles, refetch }) => {
+  const [user] = useAuthState(auth);
+  const handleOrder = (e) => {
+    e.preventDefault();
+    const profile = {
+      email: user.email,
+      education: e.target.education.value,
+      location: e.target.location.value,
+      phone: e.target.phone.value,
+      linkedin: e.target.linkedin.value,
+    };
+
+    if (profiles[0] === undefined) {
+      fetch(`http://localhost:5000/updateProfile`, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(profile),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          refetch();
+          toast('Profile Update successfully');
+        });
+    } else {
+      fetch(`http://localhost:5000/updateProfile/${profiles[0]._id}`, {
+        method: 'PUT',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(profile),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          refetch();
+          toast('Profile Update successfully');
+        });
+    }
+  };
+  return (
+    <div>
+      <input type='checkbox' id='profile-modal' className='modal-toggle' />
+      <div className='modal modal-bottom sm:modal-middle'>
+        <div className='modal-box'>
+          <label htmlFor='profile-modal' className='btn btn-sm btn-circle absolute right-2 top-2'>
+            ✕
+          </label>
+          <h3 className='font-bold text-lg'>Update Your Profile</h3>
+          <form onSubmit={handleOrder}>
+            <label className='label'>
+              <span className='label-text'>Education</span>
+            </label>
+            <input type='text' name='education' placeholder='Your Education' className='input w-full max-w-xs' />
+            <label className='label'>
+              <span className='label-text'>Location</span>
+            </label>
+            <input type='text' name='location' placeholder='Your Location' className='input w-full max-w-xs' />
+            <label className='label'>
+              <span className='label-text'>Phone Number</span>
+            </label>
+            <input type='text' name='phone' placeholder='Phone Number' className='input w-full max-w-xs' />
+            <label className='label'>
+              <span className='label-text'>LinkedIn Id</span>
+            </label>
+            <input type='text' name='linkedin' placeholder='Your LinkedIn Id' className='input w-full max-w-xs' />
+            <input type='submit' value='Update Profile' className='btn btn-primary w-full max-w-xs' />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UpdateProfileModal;
